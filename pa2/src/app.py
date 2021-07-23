6604173e233c524d2e45b3aa2bda3a972e5fedff
Sanjit Basker <tijnasreksab@gmail.com>
2021-07-23 19:36:40 -0400

    finished pa2

--- pa2/src/app.py
+++ pa2/src/app.py
@@ -6,6 +6,15 @@ DB = db.DatabaseDriver()
 
 app = Flask(__name__)
 
+def myHash(word):
+    return word
+
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
 
 @app.route("/")
 def hello_world():
@@ -14,24 +23,58 @@ def hello_world():
 
 # your routes here
 @app.route("/api/users/", methods=["GET"])
-def get_all_users():
-    pass
+def get_users():
+    return success_response(DB.get_all_users())
 
 @app.route("/api/users/", methods=["POST"])
 def create_user():
-    pass
+    body = json.loads(request.data)
+    name = body.get("name")
+    username = body.get("username")
+    passwordhash = myHash(body.get("password", "password"))
+    balance = body.get("balance", 0)
+    if not (name and username):
+        return failure_response("not enough information", 400)
 
-@app.route("/api/users/<int:user_id>", methods=["GET"])
+    user = DB.insert_user_table(name, username, passwordhash, balance)
+    return success_response(user)
+
+
+@app.route("/api/user/<int:user_id>/", methods=["GET"])
 def get_user(user_id):
-    pass
+    user = DB.get_user_by_id(user_id)
+    if user:
+        return success_response(user)
+    return failure_response("user not found")
 
-@app.route("/api/users/<int:user_id>", methods=["DELETE"])
+
+@app.route("/api/user/<int:user_id>/", methods=["DELETE"])
 def delete_user(user_id):
-    pass
+    user = DB.get_user_by_id(user_id)
+    if user:
+        DB.delete_user_by_id(user_id)
+        return success_response(user)
+    return failure_response("user not found", code=400)
 
-@app.route("/api/send/")
+
+@app.route("/api/send/", methods=["POST"])
 def send_money():
-    pass
+    body = json.loads(request.data)
+    id1 = body.get("sender_id")
+    id2 = body.get("receiver_id")
+    change = body.get("amount")
+    if id1 is not None and id2 is not None and change:
+        usr1bal = DB.get_user_by_id(id1)["balance"]
+        if usr1bal < change:
+            return failure_response("not enough monies", 400)
+        DB.change_balance(id1, -change)
+        DB.change_balance(id2, change)
+        return success_response({
+            "sender_id": id1, 
+            "receiver_id": id2,
+            "amount": change})
+    return failure_response("not enough inputs")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- pa2/src/db.py
+++ pa2/src/db.py
@@ -20,4 +20,85 @@ class DatabaseDriver(object):
     """
 
     def __init__(self):
-        pass
+        self.conn = sqlite3.connect('user.db', check_same_thread=False)
+        self.create_user_table()
+
+    def create_user_table(self):
+        self.conn.execute("DROP TABLE IF EXISTS user;")
+        self.conn.commit()
+        self.conn.execute("""
+            CREATE TABLE user (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                username TEXT NOT NULL,
+                passwordhash TEXT,
+                balance INTEGER NOT NULL
+            );
+        """)
+        self.conn.commit()
+    
+    def get_all_users(self):
+        users = self.conn.execute("""
+            SELECT * FROM user;
+        """)
+        users_list = []
+        for row in users:
+            user = {}
+            user["id"] = row[0]
+            user["name"] = row[1]
+            user["username"] = row[2]
+            users_list.append(user)
+        return users_list
+
+    def insert_user_table(self, name, username, passwordhash, balance=0):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            INSERT INTO user (name, username, passwordhash, balance) 
+            VALUES (?,?,?,?);
+        """, (name, username, passwordhash, balance))
+        self.conn.commit()
+        return self.get_user_by_id(cursor.lastrowid)
+    
+    def get_user_by_id(self, user_id):
+        cursor = self.conn.execute("""
+            SELECT * from user WHERE id = ?;
+        """, (user_id,))
+        for row in cursor:
+            print("found")
+            user = {}
+            user["id"] = row[0]
+            user["name"] = row[1]
+            user["username"] = row[2]
+            user["balance"] = row[4]
+            return user
+        print("notfound")
+        return None
+
+    def delete_user_by_id(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            DELETE FROM user WHERE id = ?;
+        """, (id,))
+        self.conn.commit()
+
+    def change_balance(self, id, change):
+        cursor = self.conn.execute("""
+            SELECT * FROM user where id = ?;
+        """, (id,))
+        currentBal = None
+        for row in cursor:
+            currentBal = row[4]
+            currentBal += change
+        
+        if currentBal is not None:
+            cursor = self.conn.cursor()
+            cursor.execute("""
+            UPDATE user SET BALANCE = ?
+            WHERE id = ?;
+            """, (currentBal, id))
+            self.conn.commit()
+            return self.get_user_by_id(id)
+        else:
+            return None
+
+    
